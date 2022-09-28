a72b5bef7e81cb6b6386eb8c11505687b9c48529
Edgar Muradyan <90352083+Edgar044@users.noreply.github.com>
2022-09-28 16:05:23 +0400

    Add files via upload

---
+++ Builder/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+class carBuilder
+{
+	//main
+	void buildBody(){std::cout<<"car body created";}
+	void buildDoors(){std::cout<<"car Doors in place";}
+	void buildCoolor(){std::cout<<"car is painting";}
+	void buildTiers(){std::cout<<"car tiers in place";}
+    void buildEngine(){std::cout<<"Engine ready to work";}
+
+	//optional
+	void buildMultimedia(){std::cout<<"Multimedia is install";}
+	void buildBoardComputer(){std::cout<<"Board Computer ready";}
+	void buildTRC(){std::cout<<"treaction control ready to save you in roada";}
+	void buildABS(){std::cout<<"ABS is included on your car";}
+
+
+
+};
+
+
+
